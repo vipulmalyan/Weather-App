67c1df5012d25c9b2ecd9d2e57d70e6d244d0acb
Vipul Malyan <143522091+vipulmalyan@users.noreply.github.com>
2023-12-31 03:11:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -78,11 +78,8 @@ footer_html = """
 """
 st.markdown(footer_html, unsafe_allow_html=True)
 
-# Add custom CSS to hide the GitHub icon
-hide_github_icon = """
-#GithubIcon {
-  visibility: hidden;
-}
-"""
-st.markdown(hide_github_icon, unsafe_allow_html=True)
+hide_github_icon = “”"
 
+.css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob, .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137, .viewerBadge_text__1JaDK{ display: none; } #MainMenu{ visibility: hidden; } footer { visibility: hidden; } header { visibility: hidden; }
+“”"
+st.markdown(hide_github_icon, unsafe_allow_html=True)
