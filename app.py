f8ed57ead6ae88b87f4f632cc7d0bf5f3e6069b8
Vipul Malyan <143522091+vipulmalyan@users.noreply.github.com>
2023-12-31 03:15:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -77,16 +77,3 @@ footer_html = """
 <div class="footer">Made by Vipul Malyan</div>
 """
 st.markdown(footer_html, unsafe_allow_html=True)
-
-st.markdown(
-    """
-    <style>
-    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
-    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
-    .viewerBadge_text__1JaDK {
-        display: none;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
