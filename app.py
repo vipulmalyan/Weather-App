302e4ea72f484354517b4b5459d89b8f7f8f6289
Vipul Malyan <143522091+vipulmalyan@users.noreply.github.com>
2023-12-31 03:11:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -78,7 +78,7 @@ footer_html = """
 """
 st.markdown(footer_html, unsafe_allow_html=True)
 
-hide_github_icon = “”"
+hide_github_icon = ""
 
 .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob, .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137, .viewerBadge_text__1JaDK{ display: none; } #MainMenu{ visibility: hidden; } footer { visibility: hidden; } header { visibility: hidden; }
 “”"
